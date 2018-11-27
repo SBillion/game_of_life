5223511b864a04385f13b05c9c60b820e0f87741
Sebastien Billion <sebastien.billion@gmail.com>
2018-11-27 14:01:44 +0100

    Use argument to set the positionof initial life

--- main.py
+++ main.py
@@ -2,16 +2,13 @@ import argparse
 
 from universe import Universe
 
-
-
-
-
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Runs Conway's Game of Life simulation.")
+    parser = argparse.ArgumentParser(
+        description="Runs Conway's Game of Life simulation.")
     parser.add_argument(
         '--size',
         dest='size',
+        type=int,
         required=False,
         default=100,
         help='Size of the universe'
@@ -19,13 +16,16 @@ if __name__ == "__main__":
     parser.add_argument(
         "--figure_name",
         dest='figure_name',
+        type=str,
         choices=['10_cells_row']
     )
     parser.add_argument(
         "--figure_position",
         dest='figure_position',
-        type=list
+        help='Top left corner coordinates of the figure in the universe. 2 '
+             'numbers seperates by a coma'
     )
     args = parser.parse_args()
+    figure_position = list(args.figure_position.split(','))
 
-    universe = Universe(args.size,args.figure_name)
+    universe = Universe(args.size, args.figure_name, figure_position)
